086bbfdfe2d6a72e76154c136155879d053fe832
christina <916823434@qq.com>
2021-05-18 18:22:29 +0800

    new driver

--- exp2/dllist-driver.cc
+++ exp2/dllist-driver.cc
@@ -1,28 +1,45 @@
-#include <cstddef>
-#include"dllist.h"
-#include<stdio.h>
-#include<stdlib.h>
-#include<iostream>
+// dllist-dirver.cc
+//  Provide 2 functions to help operate doubly-linked list.
+//
+//  You can specify the list and the number of items.
+//
+// Copyright (c) 2020 Marx Young. All rights reserved.
+
+#include "dllist.h"
 #include "system.h"
 
-void GenerateN(int N, DLList *list) {
+//----------------------------------------------------------------------
+// GenerateN
+// 	Generates N items with random keys and inserts them into a
+//  doubly-linked list.
+//----------------------------------------------------------------------
+
+void
+GenerateN(int N, DLList *list) {
     while (N--) {
-        int key = rand() % 2001 - 1000;   // here we limit the range
+        int key = Random() % 2001 - 1000;   // here we limit the range
                                 // of random numbers to [ -1000, 1000 ]
                                 // just for the convenience of demonstration
-        int item = rand();
+        int item = Random();
         list->SortedInsert(&item, key);
         list->Show();
         printf("Insert an item which key is %d\n", key);
     }
 }
 
-void RemoveN(int N, DLList *list) {
+//----------------------------------------------------------------------
+// RemoveN
+// 	Remove N items starting from the head of the list
+//  and prints out the removed items to the console.
+//----------------------------------------------------------------------
+
+void
+RemoveN(int N, DLList *list) {
     int key;
     int *item_ptr;
     while (N--) {
         item_ptr = (int *)list->Remove();
-        //key = *item_ptr;
+        key = *item_ptr;
         list->Show();
         if (item_ptr) {
             printf("Remove an item which key is %d\n", key);
@@ -31,17 +48,4 @@ void RemoveN(int N, DLList *list) {
         }
     }
 }
-/*
-int main()
-{
-	int N = 10;
-	int i;
-	int s = 0, key;
-	int* item_ptr;
-	DLList pt;
-	DLList* test=&pt;
 
-	GenerateN(N,test);
-	RemoveN(N,test);
-}
-*/
